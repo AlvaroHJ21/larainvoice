8b4a10a6387a4cb803463ce95a3c5df70f1b82dc
AlvaroHJ21 <alvarohuaysara@gmail.com>
2023-11-21 13:34:21 -0500

    update taxes controller

--- app/Http/Controllers/TaxController.php
+++ app/Http/Controllers/TaxController.php
@@ -2,9 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tax;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class TaxController extends Controller
 {
-    //
+    public function index()
+    {
+        $data = Tax::all();
+        $ok = true;
+        return response()->json(compact("ok", "data"));
+    }
+
+    public function update(Request $request, Tax $tax)
+    {
+        $validator = Validator::make($request->all(), [
+            "code" => "string",
+            "name" => "string",
+            "percentage" => "numeric",
+            "type" => "string",
+            "is_active" => "boolean",
+        ]);
+
+        if ($validator->fails()) {
+            $ok = false;
+            $erros = $validator->errors()->all();
+            return response()->json(compact("ok", "errors"), 400);
+        }
+
+        $tax->update($request->all());
+
+        $ok = true;
+        $data = $tax;
+        $message = "Impuesto actualizado exitosamente";
+
+        return response()->json(compact("ok", "data", "message"));
+    }
 }
