b099e2cce10b08b72e2f9a7b85fa79f112e8f034
AlvaroHJ21 <alvarohuaysara@gmail.com>
2023-12-02 08:11:47 -0500

    Add Payment model and migration

---
+++ app/Models/Payment.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'sale_id',
+        'date',
+        'amount',
+        'observations',
+        'payment_method_id',
+    ];
+
+    protected $attributes = [
+        'observations' => null,
+    ];
+
+    public function sale()
+    {
+        return $this->belongsTo(Sale::class);
+    }
+}
